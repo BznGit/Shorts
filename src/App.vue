0ec8ec969d29f4a369c1b242038d023a931874ff
BznGit <beznyakov@gmail.com>
2024-04-19 16:22:26 +0300

    play

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,66 @@
 <script setup lang="ts">
+  import { ref } from "vue"
   import { Swiper, SwiperSlide } from "swiper/vue";
   import "swiper/css/pagination";
   import "swiper/css";
   import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
   
   const modules = [ Navigation, Pagination, Scrollbar, A11y ]
-  const onSwiper = (swiper: any) => {
-   console.log(swiper.el.querySelector('video').play())
-    swiper.el.querySelector('video').play()
-      };
-  const onSlideChange = (e: any) => {
-
-    const currentSlide: any = e.slides[e.activeIndex]
-    currentSlide.querySelector('video').play()
-  };
+  let play = ref(false)
   const onSlideClick =  (e: any) => {
-    console.log(e,'clik')
+    if(play.value == true){
+      const currentSlide: any = e.slides[e.activeIndex]
+      currentSlide.querySelector('video').play()
+      play.value = false
+    }else{
+      const currentSlide: any = e.slides[e.activeIndex]
+      currentSlide.querySelector('video').pause()
+      play.value = true
+    }
   }
+  const onSlideChange = (e: any) => {
+    if(!play.value){
+      e.slides.forEach((item: any) => item.querySelector('video').pause())
+      const currentSlide: any = e.slides[e.activeIndex]
+      currentSlide.querySelector('video').play()
+    }else return
+
+  };
 
 </script>
 
 <template>
+
 <div class="swiper">
   <!-- Additional required wrapper -->
   <div class="swiper-wrapper">
+   
     <swiper
       :slides-per-view="1"
       :space-between="10"
       :modules="modules"
       :direction="'vertical'"
-      @swiper="onSwiper"
       @slideChange="onSlideChange"
       @click="onSlideClick"
-    >
-      <swiper-slide   >
-        <video 
-        >
-        <source src="/video/1.mp4" type='video/mp4' />
-      </video>
+    >  
+      <swiper-slide>
+        <div v-if="play"  class="stop">stop</div>
+        <video>
+          <source src="/video/1.mp4" type='video/mp4' />
+        </video>
       </swiper-slide>
-      <swiper-slide >
-        <video 
-
-        >
-        <source src="/video/2.mp4" type='video/mp4' />
-      </video>
+      <swiper-slide>
+        <div v-if="play" class="stop">stop</div>
+        <video>
+          <source src="/video/2.mp4" type='video/mp4' />
+        </video>
       </swiper-slide>
-
-      <swiper-slide >
-        <video 
-        >
-        <source src="/video/3.mp4" type='video/mp4' />
-      </video>
+      <swiper-slide>
+        <div v-if="play" class="stop">stop</div>
+        <video>
+          <source src="/video/3.mp4" type='video/mp4' />
+        </video>
       </swiper-slide>
-      
     </swiper>
   </div>
 </div>
