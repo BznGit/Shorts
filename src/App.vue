11a4039e826631c4c01d76197a376975261581f5
BznGit <beznyakov@gmail.com>
2024-04-22 12:47:28 +0300

    some

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <script setup lang="ts">
-  import { ref } from "vue"
+  import { ref, onMounted } from "vue"
   import { Swiper, SwiperSlide } from "swiper/vue";
   import "swiper/css/pagination";
   import "swiper/css";
   import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
   
+  onMounted(() => {
+    const swp: any = document.querySelector('.swiper');
+    console.log(swp)
+    const currentSlide: any = swp.slides[0]
+      let video =  currentSlide.querySelector('video')
+      if(video.height > video.width){
+        video.classList.remove('vid')
+        video.classList.add('normalized')
+      } else {
+        video.classList.remove('normalized')
+        video.classList.add('vid')
+      }
+})
   const modules = [ Navigation, Pagination, Scrollbar, A11y ]
-  let play = ref(false)
+  let play = ref(true)
   const onSlideClick =  (e: any) => {
+    console.log(play.value)
     if(play.value == true){
       const currentSlide: any = e.slides[e.activeIndex]
-      currentSlide.querySelector('video').play()
+      let video =  currentSlide.querySelector('video')
+      if(video.height > video.width){
+        video.classList.remove('vid')
+        video.classList.add('normalized')
+      } else {
+        video.classList.remove('normalized')
+        video.classList.add('vid')
+      }
+      video.play()
+
       play.value = false
     }else{
       const currentSlide: any = e.slides[e.activeIndex]
@@ -22,7 +45,16 @@
     if(!play.value){
       e.slides.forEach((item: any) => item.querySelector('video').pause())
       const currentSlide: any = e.slides[e.activeIndex]
-      currentSlide.querySelector('video').play()
+      let video = currentSlide.querySelector('video')
+      if(video.height > video.width){
+        video.classList.remove('vid')
+        video.classList.add('normalized')
+      } else{
+        video.classList.remove('normalized')
+        video.classList.add('vid')
+      }
+      video.currentTime = 0
+      video.play()
     }else return
 
   };
@@ -44,20 +76,20 @@
       @click="onSlideClick"
     >  
       <swiper-slide>
-        <div v-if="play"  class="stop">stop</div>
-        <video>
+        <img v-if="play" src="./assets/svg/play.svg" class="pause">
+        <video class="vid" loop> 
           <source src="/video/1.mp4" type='video/mp4' />
         </video>
       </swiper-slide>
       <swiper-slide>
-        <div v-if="play" class="stop">stop</div>
-        <video>
+        <img v-if="play" src="./assets/svg/play.svg" class="pause">
+        <video class="vid" loop>
           <source src="/video/2.mp4" type='video/mp4' />
         </video>
       </swiper-slide>
       <swiper-slide>
-        <div v-if="play" class="stop">stop</div>
-        <video>
+        <img v-if="play" src="./assets/svg/play.svg" class="pause">
+        <video class="vid" loop>
           <source src="/video/3.mp4" type='video/mp4' />
         </video>
       </swiper-slide>
